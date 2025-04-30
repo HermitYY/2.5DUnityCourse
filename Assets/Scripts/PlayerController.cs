e1f770ec9983fa4a10e27ba7c20d25a2a9c8ef4b
Y <hermityayo@gmail.com>
2025-04-30 14:02:00 +0800

    项目初始化

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float x = playerControls.Player.Move.ReadValue<Vector2>().x;
+        float z = playerControls.Player.Move.ReadValue<Vector2>().y;
+        Debug.Log(x + "," + z);
+    }
+}
